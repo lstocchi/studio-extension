containers/podman-desktop-extension-ai-lab

fix: upload model on podman machine on WSL to speed loading
Signed-off-by: lstocchi <lstocchi@redhat.com>

packages/backend/src/managers/applicationManager.ts
@@ -121,10 +121,17 @@ export class ApplicationManager {
       const configAndFilteredContainers = this.getConfigAndFilterContainers(recipe.config, localFolder);
 
       // get model by downloading it or retrieving locally
-      const modelPath = await this.modelsManager.downloadModel(model, {
+      let modelPath = await this.modelsManager.downloadModel(model, {
         'recipe-id': recipe.id,
         'model-id': model.id,
       });
+
+      // upload model to podman machine if user system is supported
+      modelPath = await this.modelsManager.uploadModelToPodmanMachine(model, modelPath, {
+        'recipe-id': recipe.id,
+        'model-id': model.id,
+      });
+
 
       // build all images, one per container (for a basic sample we should have 2 containers = sample app + model service)
       const images = await this.buildImages(

lstocchi/studio-extension

packages/backend/src/managers/applicationManager.ts
@@ -133,10 +133,17 @@ export class ApplicationManager extends Publisher<ApplicationState[]> {
       const configAndFilteredContainers = this.getConfigAndFilterContainers(recipe.config, localFolder);
 
       // get model by downloading it or retrieving locally
-      const modelPath = await this.modelsManager.requestDownloadModel(model, {
+      let modelPath = await this.modelsManager.requestDownloadModel(model, {
         'recipe-id': recipe.id,
         'model-id': model.id,
       });
+
+      // upload model to podman machine if user system is supported
+      modelPath = await this.modelsManager.uploadModelToPodmanMachine(model, modelPath, {
+        'recipe-id': recipe.id,
+        'model-id': model.id,
+      });
+
 
       // build all images, one per container (for a basic sample we should have 2 containers = sample app + model service)
       const images = await this.buildImages(